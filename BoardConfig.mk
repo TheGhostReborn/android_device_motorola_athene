144971382455c723542cdd49b8191c0aaad3d391
TheGhostReborn <abhinandammanamkandy2001@gmail.com>
2022-07-12 19:12:15 +0530

    athene: build broken

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,10 @@ TARGET_CPU_SMP := true
 
 TARGET_CPU_CORTEX_A53 := true
 
+# Build broken
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_USES_BUILD_HOST_EXECUTABLE := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
