41140622a41e4a6c44a74c714932ec003c7c87b6
TheGhostReborn <abhinandammanamkandy2001@gmail.com>
2022-07-13 20:09:02 +0530

    athene: drop radio hals

--- hidl.mk
+++ hidl.mk
@@ -88,11 +88,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-#RIL
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1 \
-    android.hardware.radio.config@1.0
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
